a3e1c168ab3df1174785dac6499ad0840eff4ade
ChangTraiNamAy2405 <chunghoa1122@gmail.com>
2023-04-01 21:08:18 +0700

    create the configuration and change something to understand

--- core/Config.php
+++ core/Config.php
@@ -5,18 +5,19 @@ namespace Core;
 class Config {
   private static $config = [
     'version'                 => '0.0.1',
-    'root_dir'                => '/cms',    // se la dau / trong live server
+    'root_dir'                => '/cms',    // se la dau / trong live server (path)
     'default_controler'       => 'Blog',    // default home controler
     'default_layout'          => 'default', // default layout that is used
     'default_site_title'      => 'Freeskills', // Default site title
-    'db_host'                 => '127.0.0.1',   // Db host use IP adress bot domain 
-    'db_name'                 => 'cms',       // DB name
+    'db_host'                 => '127.0.0.1',   // Db host use IP adress not domain (quick access to db)
+    'db_name'                 => 'cms',       // DB name 
     'db_user'                 => 'root',      // DB user
     'db_pasword'              => '',          // DB password
     'login_cookie_name'       => 'fhsdfs3424njsdf'    // Login cookie name
   ];
 
-  public static function get ($key) {
+  public static function get ($key) {       //get the value inside of Config.
     return array_key_exists($key, self::$config)? self::$config[$key]  : NULL;
-  }
+  }//kiem tra xem key or index co ton tai trong array hay khong. ham tra ra ket qua true or false.
+  //neu true tra ra value, neu false tra ra null.
 }
--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 session_start();
-use \Core\Config;
 
 // define constants
-define('PROOT', __DIR__);
-define('DS', DIRECTORY_SEPARATOR);
-// var_dump(DS);
+define('PROOT', __DIR__);   //__dir__ show duong dan cua file.
+define('DS', DIRECTORY_SEPARATOR);  // show dau gach cheo "\"
 
-spl_autoload_register (function($className) {
-  $parts = explode('\\', $className);
-  $class = end($parts);
-  array_pop($parts);
-  $path = strtolower(implode(DS, $parts));
-  $path = PROOT . DS . $path . DS . $class . '.php';
-  if (file_exists($path)) {
-    include($path);
-  }
-  // var_dump($path);
-});
-$db_name = Config::get('db_name');
-var_dump($db_name);
 
 ?>
