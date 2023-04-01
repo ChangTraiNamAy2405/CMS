bc3b7d789c303c379f4ccd50609c348f6fbd3485
ChangTraiNamAy2405 <chunghoa1122@gmail.com>
2023-04-01 22:07:09 +0700

    finish the autoloaded class

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 session_start();
-
+use \Core\Config;   // su dung file namespace la Core va dung ham Config tranh trung ten voi cac ham khac
 // define constants
 define('PROOT', __DIR__);   //__dir__ show duong dan cua file.
 define('DS', DIRECTORY_SEPARATOR);  // show dau gach cheo "\"
+// var_dump(DS);
 
+// auto loaded classes (khong can phai include or require moi lan dung den)
+spl_autoload_register(function($className) {
+  $parts = explode('\\', $className);      //explode bien string thanh day ngan cach nhau bang ki tu trong string.
+  $class = end($parts); // lay ten class bang gia tri cuoi cung cua $parts
+  array_pop($parts); // sau khi lay duoc ten class se remove chinh class trong day $parts;
+  $path = strtolower(implode(DS, $parts));
+  $path = PROOT . DS . $path. DS . $class . '.php';
+  if(file_exists($path)) {
+    include($path);
+  }
+  var_dump($path); // tach nhung phan trong phan use dau trang.
+});
 
+$db_name = Config::get('db_name');
+var_dump($db_name);
 ?>
